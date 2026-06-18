e25be866679625e3cf6377355788ac6e25e7a316
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-06-18 18:15:37 -0500

    feat: change the color of border

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -96,7 +96,7 @@ hl.config({
         border_size = 2,
 
         col = {
-            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
+	    active_border   = { colors = {"rgba(705959aa)", "rgba(705959aa)"}, angle = 45 },
             inactive_border = "rgba(595959aa)",
         },
 
