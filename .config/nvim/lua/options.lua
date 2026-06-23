c46546e5ea8b19418b920d753d9a6fe545a0f1a6
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-06-22 19:14:27 -0500

    feat(nvim): configuring tabs and indentation

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,3 +4,13 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+local opt = vim.opt
+
+opt.expandtab = false
+
+opt.tabstop = 8
+
+opt.shiftwidth = 8
+
+opt.softtabstop = 8
